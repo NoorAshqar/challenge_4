238db76442e5912d121cfba6a754eb7f90061658
Nourasqar <noorashqar123@gmail.com>
2020-11-05 16:34:37 +0200

    goodbye my life

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,13 @@
 
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./component1";
+
+const test = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  test
+);
--- server.js
+++ server.js
@@ -1,2 +1,11 @@
+const { request } = require("http");
 
+const express = require('express')
+const app = express()
+const path = require('path')
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/public/index.html'))
+})
+
+app.listen(3000)
